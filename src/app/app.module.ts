204503c31a92e077a08325fa1fd2337217ce5bd2
Anderson <sonvicente@hotmail.com>
2020-07-04 22:31:05 -0300

    add intercept com in-memory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDatabase } from './in-memory-database';
+// remover qdo tiver o backend
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase) // requisisoes interceptadas
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
