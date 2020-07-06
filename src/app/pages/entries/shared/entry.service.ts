62b4a893c1c6bbc86b2fc9fd8d01b0a293231786
Anderson <sonvicente@hotmail.com>
2020-07-06 19:13:36 -0300

    adiciopnando gambs de associacao entrey /category

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -6,6 +6,7 @@ import { map, catchError, flatMap } from 'rxjs/operators';
 
 import { Entry } from './entry.model';
 
+import { CategoryService } from '../../categories/shared/category.service';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,9 @@ export class EntryService {
 
     private apiPath = 'api/entries';
 
-    constructor( private http: HttpClient ) { }
+    constructor(
+        private http: HttpClient,
+        private categoryService: CategoryService) { }
 
     getAll(): Observable<Entry[]>{
         return this.http.get(this.apiPath).pipe(
@@ -32,22 +35,55 @@ export class EntryService {
     }
 
     create(entry: Entry): Observable<Entry> {
+/*
+        A API retornaria tudo da categoria
+        gambiarra feita por causa da fake API in-memory
+        com map Observable<Observable<entry>>
+        com flatMap Observable<entry>
+*/
+        return this.categoryService.getById(entry.categoryId).pipe(
+            // flatMap achata os obsevables
+            flatMap( category => {
+                entry.category = category;
+
+                // Observable<entry>
+                return this.http.post(this.apiPath, entry).pipe(
+                    catchError(this.handelError),
+                    map(this.jsonDataToEntry)
+                );
+            })
+        );
+/*
         return this.http.post(this.apiPath, entry).pipe(
             catchError(this.handelError),
             map(this.jsonDataToEntry)
         );
+*/
     }
 
     update(entry: Entry): Observable<Entry> {
 
         const url = `${this.apiPath}/${entry.id}`;
 
+        return this.categoryService.getById(entry.categoryId).pipe(
+            flatMap( category => {
+                entry.category = category;
+
+                return this.http.put(url, entry).pipe(
+                    catchError(this.handelError),
+                    map( () => entry )
+                );
+            })
+        );
+
+/*
         return this.http.put(url, entry).pipe(
             catchError(this.handelError),
             map( () => entry )
             // map(this.jsonDataToEntry)
             // não dá pra ser assim pois o in memory não retorna nada no put
         );
+*/
     }
 
     delete(id: number): Observable<any> {
