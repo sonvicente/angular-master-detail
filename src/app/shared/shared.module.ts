40972d22b63c8f9701cf3591ca91ace9ec894e00
Anderson <sonvicente@hotmail.com>
2020-07-10 23:37:39 -0300

    breadcrumb

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 
 @NgModule({
     imports: [
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        RouterModule
     ],
-    declarations: [],
+    declarations: [
+        BreadCrumbComponent,
+    ],
     exports: [
+        /* shared modules */
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        RouterModule,
+
+        /* shared components */
+        BreadCrumbComponent,
     ]
 })
 export class SharedModule { }
